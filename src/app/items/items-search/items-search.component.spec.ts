bbef395dce2475d0fac87858c9519ce2b8299f6f
Luis Montealegre <montealegreluis@gmail.com>
2018-04-18 22:40:25 -0500

    chore: Fix test for the items search component

--- src/app/items/items-search/items-search.component.spec.ts
+++ src/app/items/items-search/items-search.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ItemsSearchComponent } from './items-search.component';
+import {AppMaterialModule} from '../../app-material-module';
+import {ItemsService} from './items.service';
+import {HttpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+class ItemsServiceStub {
+}
 
 describe('ItemsSearchComponent', () => {
   let component: ItemsSearchComponent;
@@ -8,7 +15,9 @@ describe('ItemsSearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ItemsSearchComponent ]
+      declarations: [ ItemsSearchComponent ],
+      imports: [AppMaterialModule, HttpModule, BrowserAnimationsModule],
+      providers: [{provide: ItemsService, useClass: ItemsServiceStub}]
     })
     .compileComponents();
   }));
